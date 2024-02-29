0f50058827ad59397c5655b5d3ab8ffb1e3ee88d
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-02-29 10:25:20 +0500

    Update AdminRegister_page.jsx

--- client/src/components/authentication_components/AdminRegister_page.jsx
+++ client/src/components/authentication_components/AdminRegister_page.jsx
@@ -6,7 +6,7 @@ import {adminRegisterSchema} from '../../schemas';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const serverUrl = 'https://babulmadina.up.railway.app';
+//const serverUrl = 'https://babulmadina.up.railway.app';
  
 const AdminRegister_page = () => {
 
@@ -23,7 +23,7 @@ const AdminRegister_page = () => {
 		const Data = event;
 		const { name, email, contact, password, conPassword } = Data;
 
-		const res = await fetch(`${serverUrl}/admin_register`, {
+		const res = await fetch(`https://babulmadina.up.railway.app/admin_register`, {
 			method: "POST",
 			headers: {
 				Accept: "application/json",
