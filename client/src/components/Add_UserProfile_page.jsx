722626c6a62c5ad4bafd81fee9db5d3990db38db
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-03-08 12:01:51 +0500

    Update Add_UserProfile_page.jsx

--- client/src/components/Add_UserProfile_page.jsx
+++ client/src/components/Add_UserProfile_page.jsx
@@ -11,7 +11,7 @@ import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const serverUrl = 'http://localhost:5000';
+const serverUrl = 'https://babulmadina.up.railway.app';
 
 const Add_UserProfile_page = () => {
 
