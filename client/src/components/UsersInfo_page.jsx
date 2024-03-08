69e69dfea18e64953024950e5887fee01a2ca34c
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-03-08 12:03:10 +0500

    Update UsersInfo_page.jsx

--- client/src/components/UsersInfo_page.jsx
+++ client/src/components/UsersInfo_page.jsx
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const serverUrl = 'http://localhost:5000';
+const serverUrl = 'https://babulmadina.up.railway.app';
  
 const UsersInfo_page = () => {
 
