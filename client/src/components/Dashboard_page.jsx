ad886b982b218a15b425d990c4a0061697d471e1
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-03-12 10:34:49 +0500

    Update Dashboard_page.jsx

--- client/src/components/Dashboard_page.jsx
+++ client/src/components/Dashboard_page.jsx
@@ -5,19 +5,19 @@ import {useNavigate} from 'react-router-dom';
 
 const Dashboard_page = () => {
 
-    function reloadPage() {
-        var currentDocumentTimestamp = new Date(performance.timing.domLoading).getTime();
-        // Current Time //
-        var now = Date.now();
-        // Total Process Lenght as Minutes //
-        var tenSec = 5 * 1000;
-        // End Time of Process //
-        var plusTenSec = currentDocumentTimestamp + tenSec;
-        if (now > plusTenSec) {
-            window.location.reload();
-        }
-    }
-    reloadPage();
+    // function reloadPage() {
+    //     var currentDocumentTimestamp = new Date(performance.timing.domLoading).getTime();
+    //     // Current Time //
+    //     var now = Date.now();
+    //     // Total Process Lenght as Minutes //
+    //     var tenSec = 5 * 1000;
+    //     // End Time of Process //
+    //     var plusTenSec = currentDocumentTimestamp + tenSec;
+    //     if (now > plusTenSec) {
+    //         window.location.reload();
+    //     }
+    // }
+    // reloadPage();
 
     const history = useNavigate();
     const [userData, setUserData] = useState('');
