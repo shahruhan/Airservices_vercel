ae93245d4457820e71292a5cece37e80fdea395f
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-05-03 20:02:12 +0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -13,11 +13,27 @@ import ReactDOM from "react-dom/client";
 
 // className "owl-theme" is optional
 
+// FIREBASE CONFIGURATION
+import firebase from 'firebase/compat/app';
+import "firebase/compat/storage";
+
 
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 // import { HashRouter } from 'react-router-dom';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAYV3IBu4ITaEv6n670cxVc6dYzB68WxqU",
+    authDomain: "babulmadina-9e69c.firebaseapp.com",
+    projectId: "babulmadina-9e69c",
+    storageBucket: "babulmadina-9e69c.appspot.com",
+    messagingSenderId: "100857525786",
+    appId: "1:100857525786:web:35da479a32943d30da83a3",
+    measurementId: "G-QPVF1XE5WS"
+  };
+
+  firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
